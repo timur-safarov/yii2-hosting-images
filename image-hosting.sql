d4b5f072f3059d5d75a05e36b05a059c21fda345
Timur Safarov <tisafarov@gmail.com>
2024-05-18 02:49:05 +0400

    Update 18/05/2024 9

---
+++ image-hosting.sql
@@ -0,0 +1,117 @@
+
+-- Хост: localhost
+-- Время создания: Май 17 2024 г., 22:47
+-- Версия сервера: 8.0.36
+-- Версия PHP: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `image`
+--
+
+CREATE TABLE `image` (
+  `id` int UNSIGNED NOT NULL COMMENT 'id Картинки',
+  `file_name` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Название',
+  `ext` varchar(4) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Тип файла',
+  `size` int UNSIGNED NOT NULL COMMENT 'Размер файла',
+  `created_at` int UNSIGNED NOT NULL COMMENT 'Дата создания'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) COLLATE utf8mb4_general_ci NOT NULL,
+  `apply_time` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1715712709),
+('m130524_201442_init', 1715712713),
+('m190124_110200_add_verification_token_column_to_user_table', 1715712713);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` smallint NOT NULL DEFAULT '10',
+  `created_at` int NOT NULL,
+  `updated_at` int NOT NULL,
+  `verification_token` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
+(1, 'admin', 'i9PqtriSeCoUajMpnZh01A9CnvUTW3yc', '$2y$13$TL25jlH1aq.3j.dtVQ83../Oyicu2GYkJuQGo9BkrzSR.jhehg.a2', NULL, 'admin@admin.ru', 10, 1715960418, 1715960418, 'NWDgtq5maG7sTWb9ifIR8FOaGjGZFsC7_1715960418');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `file_name` (`file_name`,`ext`);
+
+--
+-- Индексы таблицы `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id Картинки';
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
